304652bc6473ad92f6c2fb06827c473504b309be
dfokki <sonata_jjk@hotmail.com>
2017-09-23 00:26:37 +0300

    cleaned shaders code, tested geometry shaders and changing color smoothly acroos triangle

--- Oillak-Engine/Oillak-Engine/GLShaders.cpp
+++ Oillak-Engine/Oillak-Engine/GLShaders.cpp
@@ -31,8 +31,8 @@
 //	fileStream.close();
 //	return content;
 //}
-
-std::string readShaderFile(const char *filename) {
+std::string readShaderFile(const char *filename)
+{
 	std::ifstream fileIn(filename);
 	if(!fileIn.good())
 	{
@@ -44,48 +44,46 @@ std::string readShaderFile(const char *filename) {
 	fileIn.close();
 }
 
+GLuint ShaderSetUp(const char *shader_path, const int type)
+{
+
+	GLuint shader = glCreateShader(type);
+	std::string shaderStr = readShaderFile(shader_path);
+	const char *shaderSrc = shaderStr.c_str();
+	std::cout << "Compiling " << type << " shader." << std::endl;
+	glShaderSource(shader, 1, &shaderSrc, NULL);
+	glCompileShader(shader);
+
+	GLint result = GL_FALSE;
+	int logLength;
+	// Check Tess evaluation shader
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &result);
+	glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &logLength);
+	std::vector<char> shaderError((logLength > 1) ? logLength : 1);
+	glGetShaderInfoLog(shader, logLength, NULL, &shaderError[0]);
+	std::cout << &shaderError[0] << std::endl;
+
+	return shader;
+
+}
+
+
 
 GLuint LoadShader(const char *vertex_path, const char *fragment_path) {
-	GLuint vertShader = glCreateShader(GL_VERTEX_SHADER);
-	GLuint fragShader = glCreateShader(GL_FRAGMENT_SHADER);
-
-	// Read shaders
-	std::string vertShaderStr = readShaderFile(vertex_path);
-	std::string fragShaderStr = readShaderFile(fragment_path);
-	const char *vertShaderSrc = vertShaderStr.c_str();
-	const char *fragShaderSrc = fragShaderStr.c_str();
+	GLuint vao;
 
 	GLint result = GL_FALSE;
 	int logLength;
 
-	// Compile vertex shader
-	std::cout << "Compiling vertex shader." << std::endl;
-	glShaderSource(vertShader, 1, &vertShaderSrc, NULL);
-	glCompileShader(vertShader);
-
-	// Check vertex shader
-	glGetShaderiv(vertShader, GL_COMPILE_STATUS, &result);
-	glGetShaderiv(vertShader, GL_INFO_LOG_LENGTH, &logLength);
-	std::vector<char> vertShaderError((logLength > 1) ? logLength : 1);
-	glGetShaderInfoLog(vertShader, logLength, NULL, &vertShaderError[0]);
-	std::cout << &vertShaderError[0] << std::endl;
-
-	// Compile fragment shader
-	std::cout << "Compiling fragment shader." << std::endl;
-	glShaderSource(fragShader, 1, &fragShaderSrc, NULL);
-	glCompileShader(fragShader);
-
-	// Check fragment shader
-	glGetShaderiv(fragShader, GL_COMPILE_STATUS, &result);
-	glGetShaderiv(fragShader, GL_INFO_LOG_LENGTH, &logLength);
-	std::vector<char> fragShaderError((logLength > 1) ? logLength : 1);
-	glGetShaderInfoLog(fragShader, logLength, NULL, &fragShaderError[0]);
-	std::cout << &fragShaderError[0] << std::endl;
-
 	std::cout << "Linking program" << std::endl;
 	GLuint program = glCreateProgram();
-	glAttachShader(program, vertShader);
-	glAttachShader(program, fragShader);
+
+	GLuint vShader = ShaderSetUp(vertex_path, GL_VERTEX_SHADER);
+	GLuint fShader = ShaderSetUp(fragment_path, GL_FRAGMENT_SHADER);
+
+	glAttachShader(program, vShader);
+	glAttachShader(program, fShader);
+
 	glLinkProgram(program);
 
 	glGetProgramiv(program, GL_LINK_STATUS, &result);
@@ -94,105 +92,56 @@ GLuint LoadShader(const char *vertex_path, const char *fragment_path) {
 	glGetProgramInfoLog(program, logLength, NULL, &programError[0]);
 	std::cout << &programError[0] << std::endl;
 
-	glDeleteShader(vertShader);
-	glDeleteShader(fragShader);
+	glDeleteShader(vShader);
+	glDeleteShader(fShader);
 
-	return program;
-}
-
-GLuint LoadShader(const char *vertex_path, const char *tessControl_path, const char *tessEvaluation_path, const char *fragment_path)
-{
-	GLuint vao;
-
-	GLuint vertShader = glCreateShader(GL_VERTEX_SHADER);
-	GLuint fragShader = glCreateShader(GL_FRAGMENT_SHADER);
-	GLuint tesscontShader = glCreateShader(GL_TESS_CONTROL_SHADER);
-	GLuint tessevShader = glCreateShader(GL_TESS_EVALUATION_SHADER);
-	// Read shaders
-	std::string vertShaderStr = readShaderFile(vertex_path);
-	std::string tesscontShaderStr = readShaderFile(tessControl_path);
-	std::string tessevShaderStr = readShaderFile(tessEvaluation_path);
-	std::string fragShaderStr = readShaderFile(fragment_path);
-	const char *vertShaderSrc = vertShaderStr.c_str();
-	const char *tesscontShaderSrc = tesscontShaderStr.c_str();
-	const char *tessevShaderSrc = tessevShaderStr.c_str();
-	const char *fragShaderSrc = fragShaderStr.c_str();
-
-	GLint result = GL_FALSE;
-	int logLength;
-
-	// Compile vertex shader
-	std::cout << "Compiling vertex shader." << std::endl;
-	glShaderSource(vertShader, 1, &vertShaderSrc, NULL);
-	glCompileShader(vertShader);
-
-	// Check vertex shader
-	glGetShaderiv(vertShader, GL_COMPILE_STATUS, &result);
-	glGetShaderiv(vertShader, GL_INFO_LOG_LENGTH, &logLength);
-	std::vector<char> vertShaderError((logLength > 1) ? logLength : 1);
-	glGetShaderInfoLog(vertShader, logLength, NULL, &vertShaderError[0]);
-	std::cout << &vertShaderError[0] << std::endl;
-
-
-	// Compile Tesselation control shader
-	std::cout << "Compiling Tesselation control shader." << std::endl;
-	glShaderSource(tesscontShader, 1, &tesscontShaderSrc, NULL);
-	glCompileShader(tesscontShader);
-
-	// Check vertex shader
-	glGetShaderiv(tesscontShader, GL_COMPILE_STATUS, &result);
-	glGetShaderiv(tesscontShader, GL_INFO_LOG_LENGTH, &logLength);
-	std::vector<char> tesscontShaderError((logLength > 1) ? logLength : 1);
-	glGetShaderInfoLog(tesscontShader, logLength, NULL, &tesscontShaderError[0]);
-	std::cout << &tesscontShaderError[0] << std::endl;
-
-	// Compile Tesselation evaluation shader
-	std::cout << "Compiling Tesselation evaluation shader." << std::endl;
-	glShaderSource(tessevShader, 1, &tessevShaderSrc, NULL);
-	glCompileShader(tessevShader);
-
-	// Check Tess evaluation shader
-	glGetShaderiv(tessevShader, GL_COMPILE_STATUS, &result);
-	glGetShaderiv(tessevShader, GL_INFO_LOG_LENGTH, &logLength);
-	std::vector<char> tessevShaderError((logLength > 1) ? logLength : 1);
-	glGetShaderInfoLog(tessevShader, logLength, NULL, &tessevShaderError[0]);
-	std::cout << &tessevShaderError[0] << std::endl;
-
-
-	// Compile fragment shader
-	std::cout << "Compiling fragment shader." << std::endl;
-	glShaderSource(fragShader, 1, &fragShaderSrc, NULL);
-	glCompileShader(fragShader);
-
-	// Check fragment shader
-	glGetShaderiv(fragShader, GL_COMPILE_STATUS, &result);
-	glGetShaderiv(fragShader, GL_INFO_LOG_LENGTH, &logLength);
-	std::vector<char> fragShaderError((logLength > 1) ? logLength : 1);
-	glGetShaderInfoLog(fragShader, logLength, NULL, &fragShaderError[0]);
-	std::cout << &fragShaderError[0] << std::endl;
-
-	std::cout << "Linking program" << std::endl;
-	GLuint program = glCreateProgram();
-	glAttachShader(program, vertShader);
-	glAttachShader(program, tesscontShader);
-	glAttachShader(program, tessevShader);
-	glAttachShader(program, fragShader);
-	glLinkProgram(program);
-	
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
 
-	
+	return program;
+}
+
+GLuint LoadShader(const char *vertex_path, const char *tessControl_path, const char *tessEvaluation_path,const char *geometry_path, const char *fragment_path)
+{
+
+	GLuint vao;
+
+	GLint result = GL_FALSE;
+	int logLength;
+
+	std::cout << "Linking program" << std::endl;
+	GLuint program = glCreateProgram();
+
+	GLuint vShader = ShaderSetUp(vertex_path, GL_VERTEX_SHADER);
+	GLuint tcShader = ShaderSetUp(tessControl_path, GL_TESS_CONTROL_SHADER);
+	GLuint teShader = ShaderSetUp(tessEvaluation_path, GL_TESS_EVALUATION_SHADER);
+	GLuint gShader = ShaderSetUp(geometry_path, GL_GEOMETRY_SHADER);
+	GLuint fShader = ShaderSetUp(fragment_path, GL_FRAGMENT_SHADER);
+
+	glAttachShader(program,vShader);
+	glAttachShader(program, tcShader);
+	glAttachShader(program, teShader);
+	glAttachShader(program, gShader);
+	glAttachShader(program, fShader);
+
+	glLinkProgram(program);
+		
 	glGetProgramiv(program, GL_LINK_STATUS, &result);
 	glGetProgramiv(program, GL_INFO_LOG_LENGTH, &logLength);
 	std::vector<char> programError((logLength > 1) ? logLength : 1);
 	glGetProgramInfoLog(program, logLength, NULL, &programError[0]);
 	std::cout << &programError[0] << std::endl;
 
-	glDeleteShader(vertShader);
-	glDeleteShader(tesscontShader);
-	glDeleteShader(tessevShader);
-	glDeleteShader(fragShader);
+	glDeleteShader(vShader);
+	glDeleteShader(tcShader);
+	glDeleteShader(teShader);
+	glDeleteShader(gShader);
+	glDeleteShader(fShader);
+
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
 
 	return program;
 }
+
+
--- Oillak-Engine/Oillak-Engine/GLShaders.h
+++ Oillak-Engine/Oillak-Engine/GLShaders.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "Dependencies\glew\glew.h"
+#include <vector>
+
 
 GLuint LoadShader(const char *vertex_path, const char *fragment_path);
 
 GLuint LoadShader(const char *vertex_path, const char *tessControl_path, const char *tessEvaluation_path, const char *fragment_path);
+GLuint LoadShader(const char *vertex_path, const char *tessControl_path, const char *tessEvaluation_path, const char *geometry_path, const char *fragment_path);
